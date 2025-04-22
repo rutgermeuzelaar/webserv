7b87151d9c4d23d1466a071f3875b55ff1eaf04f
Linux - Thuis <rutgermeuzelaar@gmail.com>
2025-04-22 15:43:12 +0200

    Made a prototype server that acknowledges a connection.

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,87 @@
 #include <stdlib.h>
+#include <cstring>
 #include <iostream>
-#include <sys/epoll.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <csignal>
+#ifndef TEST_PORT
+# define TEST_PORT "1050"
+#endif
+
+volatile std::sig_atomic_t gSignum;
+bool	gLive = true;
+
+void signal_handler(int signal)
+{
+	gSignum = signal;
+}
 
 int main(int argc, char **argv)
 {
-	const int epoll_fd = epoll_create(1);
-	struct epoll_event event;
+	struct addrinfo hints;
+	struct addrinfo	*node;
+	struct addrinfo *server_addr;
+	int				sockfd;
+	int				peerfd;
+	int				optval;
+	socklen_t		peer_size;
+	struct sockaddr_storage peer_addr;
 	(void)argv;
-	
+	optval = 1;
 	if (argc != 2)
 	{
-		std::cout << "Insufficient arguments, please supply a configuration file.\n";
+		std::cout << "Insufficient arguments, please supply a configuration"
+		" file.\n";
 		return (EXIT_FAILURE);
 	}
-	if (epoll_fd == -1)
+	std::memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+	if (getaddrinfo(NULL, TEST_PORT, &hints, &server_addr) != 0)
 	{
 		return (EXIT_FAILURE);
 	}
-	if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, 5, &event) == -1)
+	for (node = server_addr; node != NULL; node = node->ai_next)
 	{
-		std::cout << "Hello world!\n";
+		sockfd = socket(node->ai_family, node->ai_socktype, node->ai_protocol);
+		if (sockfd == -1)
+		{
+			std::cout << "server: socket" << strerror(errno) << '\n';
+			continue;
+		}
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) == -1)
+		{
+			return (EXIT_FAILURE);
+		}
+		if (bind(sockfd, node->ai_addr, node->ai_addrlen) == -1)
+		{
+			close(sockfd);
+			std::cout << "server: bind" << strerror(errno) << '\n';
+			continue;
+		}
+		break ;
+	}
+	freeaddrinfo(server_addr);
+	if (node == NULL)
+	{
 		return (EXIT_FAILURE);
+	}
+	if (listen(sockfd, 10) == -1)
+	{
+		return (EXIT_FAILURE);
+	}
+	while (gLive)
+	{
+		peer_size = sizeof(peer_addr);
+		peerfd = accept(sockfd, (struct sockaddr*)&peer_addr, &peer_size);
+		if (peerfd == -1)
+		{
+			return (EXIT_FAILURE);
+		}
+		printf("Server received something\n");
 	}
 	return (EXIT_SUCCESS);
 }
