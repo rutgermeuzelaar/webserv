f20183497dbc301d3cd67c5792538e1fa43ab5f6
Rutger Meuzelaar <rmeuzela@student.codam.nl>
2025-05-20 20:39:46 +0200

    Worked on modeling the configuration file in a class.

--- include/ServerConfig.hpp
+++ include/ServerConfig.hpp
@@ -6,7 +6,7 @@
 /*   By: rmeuzela <rmeuzela@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/05/11 14:15:37 by rmeuzela      #+#    #+#                 */
-/*   Updated: 2025/05/11 17:13:50 by rmeuzela      ########   odam.nl         */
+/*   Updated: 2025/05/20 20:28:37 by rmeuzela      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,45 @@
 # include <string>
 # include <vector>
 # include <map>
+# include <filesystem>
+# include <optional>
+# include "Lexer.hpp"
+
+class Root
+{
+    public:
+        std::filesystem::path m_path;
+        Root();
+};
+
+class ClientMaxBodySize
+{
+    public:
+        size_t    m_size;
+        ClientMaxBodySize();
+};
+
+class Location
+{
+    public:
+        std::string m_uri;
+        std::optional<Root> m_root;
+        Location();
+};
+
+class ServerName
+{
+    public:
+        std::string m_name;
+};
 
 class ServerConfig
 {
-    private:
-    
     public:
-        std::string                             m_name;
-        std::string                             m_address;
-        int                                     m_port;
-        std::map<std::vector<int>, std::string> m_error_page;
-        std::map<std::string, std::string>      m_location;
-        size_t                                  m_client_max_body_size;
+        ServerConfig();
+        std::optional<ClientMaxBodySize> m_client_max_body_size;
+        std::optional<Location> m_location;
+        std::optional<ServerName> m_server_name;
 };
+
 #endif
