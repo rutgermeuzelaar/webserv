31904dcbd35269671716569593f11cfa3dde1ee3
robertrinh <robert.trinh@yahoo.com>
2025-06-04 14:36:04 +0200

    fixed duplicate HTTPException issue

---
+++ src/etc/HTTPException.cpp
@@ -0,0 +1,9 @@
+#include "HTTPException.hpp"
+
+HTTPException::HTTPException(HTTPStatusCode status_code, const std::string& message)
+    : std::runtime_error(message.empty() ? get_http_status_text(status_code) : message),
+      _status_code(status_code) {}
+
+HTTPStatusCode HTTPException::getStatusCode() const {
+    return _status_code;
+} 
