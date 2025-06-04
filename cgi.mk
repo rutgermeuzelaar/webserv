e9a7d4d736b4369f80c08ebdf6e763d405ed881d
Rutger Meuzelaar <rmeuzela@student.codam.nl>
2025-06-04 17:17:38 +0200

    Started working on implementing a cgi, has leaks currently.

---
+++ cgi.mk
@@ -0,0 +1,33 @@
+CC := g++
+INC_DIRS := include
+INC := $(foreach dir, $(INC_DIRS), -I$(dir))
+CFLAGS := -Wall -Wextra -Werror -Wshadow $(INC) -g -std=c++17
+LDFLAGS :=
+OBJDIR := build
+VPATH = $(shell find src/ -maxdepth 1 -mindepth 1)
+SOURCES_STANDARD := Cgi.cpp CgiTest.cpp Utilities.cpp
+NAME := cgi
+OBJECTS_STANDARD := $(SOURCES_STANDARD:%.cpp=$(OBJDIR)/%.o)
+OBJECTS_SHARED := $(SOURCES_SHARED:%.cpp=$(OBJDIR)/%.o)
+OBJECTS_BONUS := $(SOURCES_BONUS:%.cpp=$(OBJDIR)/%.o)
+
+.PHONY: all
+
+all: $(NAME)
+
+$(OBJDIR)/%.o: %.cpp | $(OBJDIR)
+	$(CC) $(CFLAGS) -c -o $@ $<
+
+$(NAME): $(OBJECTS_STANDARD) $(OBJECTS_SHARED)
+	$(CC) $(OBJECTS_STANDARD) $(OBJECTS_SHARED) -o $(NAME)
+
+$(OBJDIR):
+	mkdir $(OBJDIR)
+
+clean:
+	rm -rf $(OBJECTS_STANDARD) $(OBJECTS_SHARED) $(OBJECTS_BONUS)
+
+fclean: clean
+	rm -rf $(NAME)
+
+re: fclean all
