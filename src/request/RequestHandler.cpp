91a2c0284d6d21c48852c16c86e8f7d671fb2df7
Rutger Meuzelaar <rmeuzela@student.codam.nl>
2025-06-06 20:50:33 +0200

    Refined the request handler a bit but still needs work.

--- src/request/RequestHandler.cpp
+++ src/request/RequestHandler.cpp
@@ -6,7 +6,7 @@
 /*   By: rmeuzela <rmeuzela@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/05 14:17:11 by rmeuzela      #+#    #+#                 */
-/*   Updated: 2025/06/05 18:02:11 by rmeuzela      ########   odam.nl         */
+/*   Updated: 2025/06/06 19:38:37 by rmeuzela      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,34 +21,62 @@ RequestHandler::RequestHandler(const Config& config)
 
 }
 
+// ./root + ./root/css+stylesheet.css
+static std::filesystem::path& resolve_overlap(std::filesystem::path& root, const std::filesystem::path& to_join)
+{
+	std::filesystem::path::iterator root_it = root.begin();
+	std::filesystem::path::iterator join_it = to_join.begin();
+
+	if (*root_it == ".")
+	{
+		root_it++;
+	}
+	while (root_it != root.end() && join_it != to_join.end() && *root_it == *join_it)
+	{
+		root_it++;
+		join_it++;
+	}
+	while (join_it != to_join.end())
+	{
+		root /= *join_it;
+		join_it++;
+	}
+	return root;
+}
+
 std::optional<std::string> RequestHandler::map_uri(std::string uri)
 {
 	const ServerContext& server = m_config.m_http_context.m_servers.m_vector.at(0);
 	std::string filename;
+	std::filesystem::path uri_path(uri);
 	// get server
 	if (server.m_location_contexts.m_vector.size() == 0)
 	{
 		// defer to default value
 	}
-	if (uri.back() == '/')
+	if (uri_path.string().back() == '/')
 	{
 		// hardcode index.html
-		uri.append("index.html");
+		uri_path.append("index.html");
 	}
-	const size_t last_slash_pos = uri.rfind('/');
-	const std::string folder_path = uri.substr(0, last_slash_pos);
+	const std::string folder_path = uri_path.parent_path();
 	// if received URI like this '/' '/dir/' we look for an index.html file
 	// if it doesn't exist check if autoindex is enabled, if it isn't error otherwise generate directory listing
-	filename = uri.substr(last_slash_pos, uri.size() - last_slash_pos);
+	// filename = uri.substr(last_slash_pos, uri.size() - last_slash_pos);
 	for (auto& it :server.m_location_contexts.m_vector)
 	{
-		if (it.m_uri.find(folder_path, 0) == 0)
+		if (folder_path.compare(0, it.m_uri.size(), it.m_uri) == 0)
 		{
 			// check if it has root block
 			if (it.m_root.has_value())
 			{
-				std::string root_path = it.m_root.value().m_path.string();
-				root_path.append(filename);
+				std::filesystem::path root_path = it.m_root.value().m_path;
+				if (uri_path.is_absolute())
+				{
+					uri_path = uri_path.relative_path();
+				}
+				resolve_overlap(root_path, uri_path);
+				std::cout << root_path << '\n';
 				return std::optional<std::string>(root_path);
 			}
 		}
@@ -62,22 +90,20 @@ Response RequestHandler::handle_get(const Request& request)
 	const std::string& uri = request.getURI();
 
 	// write to the specified connection
-	m_config.m_http_context.m_servers.m_vector.at(0);
+	// m_config.m_http_context.m_servers.m_vector.at(0);
 	std::optional<std::string> local_path = map_uri(uri);
-	if (local_path.has_value())
-	{
-		Response response(HTTPStatusCode::OK);
-		response.setBodyFromFile(local_path.value());
-		std::cout << map_uri(uri).value();
-		return response;
-	}
-	else
+	if (!local_path.has_value() || !std::filesystem::exists(local_path.value()))
 	{
 		std::cout << "Could not map URI.";
 		Response response(HTTPStatusCode::NotFound);
+		response.setBodyFromFile("./root/pages/404.html");
 		return response;
-		// create 
 	}
+	Response response(HTTPStatusCode::OK);
+	response.setBodyFromFile(local_path.value());
+	std::cout << map_uri(uri).value();
+	return response;
+		// create 
 	// if file exists
 	 // if file exists but you don't have permissions
 	// if file does not exist
