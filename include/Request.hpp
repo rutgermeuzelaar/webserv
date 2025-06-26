771e897255651788124202d22173a745ce1af3e6
Rutger Meuzelaar <rmeuzela@student.codam.nl>
2025-06-26 22:47:06 +0200

    Update Request class to integrate new component classes.

--- include/Request.hpp
+++ include/Request.hpp
@@ -1,6 +1,5 @@
 #ifndef REQUEST_HPP
 # define REQUEST_HPP
-
 #include <unordered_map>
 #include <string>
 #include <sstream>
@@ -11,51 +10,39 @@
 #include "Defines.hpp"
 #include "HTTPStatusCode.hpp"
 #include "HTTPException.hpp"
-
-enum class HTTPMethod {
-	GET,
-	POST,
-	DELETE,
-	UNSUPPORTED
-};
+#include "HttpBody.hpp"
+#include "HttpMethod.hpp"
+#include "HttpRequestStartLine.hpp"
+#include "HttpHeaders.hpp"
 
 class Request
 {
-private:
-	//* status line components
-	std::string _HTTPVersion;
-	HTTPMethod	_method_type;
-	std::string _method;
-	std::string _uri;
-	std::string _path;
-	std::string _query;
-	std::string _fragment;
-	std::unordered_map<std::string, std::string>_headers; //* key-value headers
-	std::string _body;
+    private:
+        HttpRequestStartLine _start_line; //* status line components
+        HttpHeaders _headers; //* key-value headers
+        HttpBody _body;
+        std::string _raw;
+        size_t _index; // index of current byte
+        size_t _line_count; // count parsed_lines
 
-	HTTPMethod StringToMethod(const std::string& method);
-	void parseRequestLine(const std::string& line);
-	void parseHeaders(std::istream& requestStream);
-	void parseBody(std::istream& requestStream);
-	void parseURI(const std::string& uri); 
+        void parse(void);
+    public:
+        Request();
+        ~Request(); 
+        Request& operator=(const Request&);
 
-public:
-	Request();
-	~Request(); 
-	
-	void parse(const std::string& str);
+        void append(const char* buffer, size_t len);
+        bool complete(void) const;
 
-	//* getters
-	HTTPMethod getMethodType() const;
-	std::string getHTTPVersion() const;
-	std::string getMethod() const;
-	std::string getURI() const;
-	std::string getPath() const;     
-	std::string getQuery() const;
-	std::string getFragment() const;
-	std::string getHeader(const std::string& key) const;
-	std::string getBody() const;
+        //* getters
+        HttpRequestStartLine& getStartLine();
+        HttpHeaders& getHeaders();
+        HttpBody& getBody();
 
-	void printRequest() const;
+        const HttpRequestStartLine& getStartLine() const;
+        const HttpHeaders& getHeaders() const;
+        const HttpBody& getBody() const;
+
+        void printRequest() const;
 };
 #endif
