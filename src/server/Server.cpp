856ed751e724da220d25f80e2eaed7f699731c47
Quan <robert.trinh@yahoo.com>
2025-07-02 13:17:48 +0200

    removed line of the slow loading time

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -202,8 +202,7 @@ void Server::processRequest(int client_fd, const Request& request)
 void Server::sendResponseToClient(int client_fd, const Response& response)
 {
 	std::string response_str = response.to_str();
-	std::cout << "Sending response of length: " << response_str.length() << std::endl; //! TEST 
-	// std::cout << "Response content: " << response_str << std::endl; //! SLOWS DOWN THE SERVER <- because it prints out te binary data (which has to converted to ASCII)
+	std::cout << "Sending response of length: " << response_str.length() << std::endl; //! TEST
 	ssize_t bytes_sent = send(client_fd, response_str.c_str(), response_str.length(), 0);
 	if (bytes_sent == -1)
 		std::cerr << "Error sending response: " << strerror(errno) << std::endl; //! TEST 
