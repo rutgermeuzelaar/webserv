230cd3f5ae9c5123632b8d4cdce02d69fe7243ed
Rutger Meuzelaar <rmeuzela@student.codam.nl>
2025-08-04 20:09:26 +0200

    Finish Session class

--- include/Session.hpp
+++ include/Session.hpp
@@ -4,19 +4,25 @@
 # include <string>
 # include <unordered_map>
 # include <optional>
+# include "HttpMethod.hpp"
 
 class Session
 {
 	private:
 		const std::string m_id;
 		std::unordered_map<std::string, std::string> m_data;
-		
-		const std::string generate_session_id(void);
+        std::unordered_map<HTTPMethod, size_t> m_request_history;
 	public:
-		Session();
+		Session(const std::string& id);
+        Session& operator=(const Session&) = delete;
+        Session(const Session&);
+        ~Session();
 
 		const std::string& get_id(void) const;
 		void set_data_pair(const std::string& key, const std::string& value);
-		std::optional<const std::string> get_data(void) const;
+		std::optional<const std::string> get_value(const std::string& key) const;
+        const std::unordered_map<HTTPMethod, size_t>& get_request_history() const;
+        void add_request(HTTPMethod http_method);
+        void print(void) const;
 };
 #endif
--- src/server/Session.cpp
+++ src/server/Session.cpp
@@ -1,21 +1,74 @@
 #include "Session.hpp"
 #include <cstdlib>
+#include <algorithm>
+#include "HttpMethod.hpp"
 
-const std::string Session::generate_session_id()
+Session::Session(const std::string& id)
+	: m_id {id}
 {
-	static const char* hex = "0123456789abcdef";
 
-	std::string session_id;
-	session_id.reserve(32);
-	for (size_t i = 0; i < 32; ++i)
-	{
-		session_id[i] = hex[std::rand() % 16];
-	}
-	return session_id;
 }
 
-Session::Session()
-	: m_id {generate_session_id()}
+Session::Session(const Session& other)
+    : m_id {other.m_id}
+    , m_data {other.m_data}
+    , m_request_history {other.m_request_history}
 {
 
+}
+
+Session::~Session()
+{
+
+}
+
+const std::string& Session::get_id() const
+{
+    return m_id;
+}
+
+void Session::set_data_pair(const std::string& key, const std::string& value)
+{
+    m_data[key] = value;
+}
+
+std::optional<const std::string> Session::get_value(const std::string& key) const
+{
+    const auto& value = std::find_if(
+        m_data.begin(),
+        m_data.end(),
+        [&key](const std::pair<std::string, std::string>& pair) {
+            return pair.first == key;
+        }
+    );
+    if (value != m_data.end())
+    {
+        return value->second;
+    }
+    return std::nullopt;
+}
+
+void Session::add_request(HTTPMethod http_method)
+{
+    m_request_history[http_method]++;
+}
+
+void Session::print(void) const
+{
+    std::cout << "Printing data for session id: " << m_id << '\n';
+    std::cout << "m_data\n"; 
+    for (const auto& it: m_data)
+    {
+        std::cout << it.first << ": " << it.second << '\n';
+    }
+    std::cout << "m_request_history\n";
+    for (const auto& it: m_request_history)
+    {
+        std::cout << it.first << ": " << it.second << '\n';
+    }
+}
+
+const std::unordered_map<HTTPMethod, size_t>& Session::get_request_history() const
+{
+    return m_request_history;
 }
