6007a87a9008cacb3c8c73ad9fb1ecb74ff657b1
Rutger Meuzelaar <rmeuzela@student.codam.nl>
2025-08-04 20:10:03 +0200

    Add utility functions

--- include/Utilities.hpp
+++ include/Utilities.hpp
@@ -7,6 +7,29 @@
 # include <unordered_map>
 # include <filesystem>
 
+const std::unordered_map<char, std::string> g_percent_encoding_table = {
+    {':', "%3A"},
+    {'/', "%2F"},
+    {'?', "%3F"},
+    {'#', "%23"},
+    {'[', "%5B"},
+    {']', "%5D"},
+    {'@', "%40"},
+    {'!', "%21"},
+    {'$', "%24"},
+    {'&', "%26"},
+    {'\'', "%27"},
+    {'(', "%28"},
+    {')', "%29"},
+    {'*', "%2A"},
+    {'+', "%2B"},
+    {',', "%2C"},
+    {';', "%3B"},
+    {'=', "%3D"},
+    {'%', "%25"},
+    {' ', "%20"}
+};
+
 std::vector<std::string> split(const std::string to_split, const char delimiter);
 
 template <typename T> bool is_unique(const std::vector<T>& vector)
@@ -14,6 +37,22 @@ template <typename T> bool is_unique(const std::vector<T>& vector)
     for (auto it = vector.begin(); it != vector.end(); ++it)
     {
         if (std::find(std::next(it), vector.end(), *it) != vector.end())
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+template <typename Tkey, typename Tvalue> bool has_keys(const std::unordered_map<Tkey, Tvalue>& map, const std::vector<Tkey>& keys)
+{
+    if (map.size() < keys.size())
+    {
+        return false;
+    }
+    for (const auto& it: keys)
+    {
+        if (map.find(it) == map.end())
         {
             return false;
         }
@@ -32,4 +71,5 @@ std::pair<std::string, std::string> parse_single_http_header(const std::string& 
 std::unordered_map<std::string, std::string> parse_http_headers(const std::string& str, size_t* chars_consumed);
 void copy_str_bytes(std::vector<std::byte>& bytes, const std::string& str);
 std::filesystem::path map_uri_helper(std::filesystem::path root_path, std::filesystem::path& uri_path);
+std::unordered_map<std::string, std::string> parse_query_string(const std::string& str);
 #endif
