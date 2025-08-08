1fb3b54683ec9ec8024ad53aaff3e95be914945e
rmeuzela <rmeuzela@student.codam.nl>
2025-08-08 15:24:52 +0200

    Create correct responses in CGI scripts

--- root/cgi-bin/delete-entry.py
+++ root/cgi-bin/delete-entry.py
@@ -1,24 +1,41 @@
 import os
 import pathlib
 
+LINE_BREAK = "\r\n"
+REQUEST_METHOD = os.environ.get("REQUEST_METHOD")
+PATH_TRANSLATED = os.environ.get("PATH_TRANSLATED")
+
 def create_html_button(href: str, text: str) -> None:
     print(
         f"<button onclick=\"deleteRequest('/{href}')\">{text}</button"
         )
-print("Content-Type: text/html", end="\r\n")
-print(end="\r\n")
-PATH_TRANSLATED = os.environ.get("PATH_TRANSLATED")
+
+def create_response_page(status_code: int, status_message: str) -> None:
+    print(f"Status: {status_code}", end=LINE_BREAK)
+    print("Content-Type: text/plain", end=LINE_BREAK)
+    print("Status: 403", end=LINE_BREAK)
+    print(end=LINE_BREAK)
+    print(f"{status_code} - {status_message}")
+
+if REQUEST_METHOD != "GET":
+    print("Allow: GET", end=LINE_BREAK)
+    create_response_page(405, "Method Not Allowed")
+    exit(0)
 
 if PATH_TRANSLATED == None:
+    create_response_page(400, "Bad Request")
     print("Please supply a PATH_INFO environment variable")
-    exit(1)
+    exit(0)
 
 if PATH_TRANSLATED == './root':
-    exit(1)
+    create_response_page(403, "Forbidden")
+    exit(0)
 
 cwd = pathlib.Path.cwd()
 dir = cwd.joinpath(PATH_TRANSLATED)
 
+print("Content-Type: text/html", end="\r\n")
+print(end="\r\n")
 print(
     "<!DOCTYPE html>"
         "<html lang=\"en-US\">"
--- root/cgi-bin/hello-world.pl
+++ root/cgi-bin/hello-world.pl
@@ -1,3 +1,12 @@
+if ($ENV{REQUEST_METHOD} ne "GET")
+{
+    print "Status: 405\r\n";
+    print "Allow: GET\r\n";
+    print "Content-Type: text/plain\r\n\r\n";
+    print "405 - Method Not Allowed";
+    exit 0;
+}
+
 print "Content-Type: text/html\r\n";
 print "\r\n";
 print "Hello world!\n";
